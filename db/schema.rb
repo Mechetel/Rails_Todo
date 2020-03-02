0652f60895ae7f1b8b7fd96be370b3c7f83ebe8f
mechetel <dima.homa5@gmail.com>
2020-03-02 21:47:49 +0200

    reset repo to init state

--- db/schema.rb
+++ db/schema.rb
@@ -15,12 +15,4 @@ ActiveRecord::Schema.define(version: 2017_05_09_232014) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "posts", force: :cascade do |t|
-    t.string "title"
-    t.text "body"
-    t.boolean "published"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
 end
